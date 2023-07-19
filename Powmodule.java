0b9be5cfa1a0d8a12c471e43083f6f39f5266b08
JAYAPRASHANT <jayaprashanth.20eee@gmail.com>
2023-07-18 21:53:54 -0700

    prgm

---
+++ Powmodule.java
@@ -0,0 +1,27 @@
+package powerwithmodule;
+import java.util.Scanner;
+public class Powmodule {
+     public static int pow(int A,int B,int C) {
+    	int result=1;
+    	for(int i=1;i<=B;i++) {
+    		A=A%C;
+    		{
+    			result=(result*A)%C;
+    		}
+    		
+    	}
+		return result;
+    	 
+     }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Scanner sc=new Scanner(System.in);
+    int A=sc.nextInt();
+    int B=sc.nextInt();
+    int C=sc.nextInt();
+    System.out.println(pow(A,B,C));
+    
+    
+;	}
+
+}
